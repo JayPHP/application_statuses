36429d5915815a75c17f442e118b6c5e0f2fbf8d
james.byrne <JayPHP@users.noreply.github.com>
2016-07-01 10:03:08 +0100

    CS corrections

--- block_application_statuses.php
+++ block_application_statuses.php
@@ -7,54 +7,54 @@
  * @author James Byrne
  */
 
-class block_application_statuses extends block_list 
+class block_application_statuses extends block_list
 {
-    public function init() 
+    public function init()
     {
         $this->title = get_string('pluginname', 'block_application_statuses');
     }
 
-    public function specialization() 
+    public function specialization()
     {
         if (isset($this->config)) {
             if (!empty($this->config->title)) {
                 $this->title = $this->config->title;
-            }  
+            }
         }
     }
 
-    public function get_content() 
+    public function get_content()
     {
         if ($this->content !== null) {
-        	return $this->content;
+            return $this->content;
         }
 
         $this->content = new stdClass;
         $this->getApplications();
 
-    	return $this->content;
+        return $this->content;
     }
 
     /**
      * Gets applciations from db for user. Sets public $content to null if no applications found.
      *
-     * @return null 
+     * @return null
      */
     private function getApplications()
     { 
-    	global $DB, $USER;
+        global $DB, $USER;
 
-    	if ($applications = $DB->get_records('block_application_statuses', ['userid' => $USER->id])) {
-    		if (empty($applications)) {
-    			$this->content = null;
-    			return;
-    		}
+        if ($applications = $DB->get_records('block_application_statuses', ['userid' => $USER->id])) {
+            if (empty($applications)) {
+                $this->content = null;
+                return;
+            }
 
-    		$this->content->items = array();
-    		foreach ($applications as $application) {
-    			$this->content->items[] = html_writer::tag('a', $application->prospect_title, ['href' => $application->link, 'target' => '_blank', 'class' => 'application-prospect']);
-    			$this->content->items[] = html_writer::div($application->status);
-    		}
-    	}
+            $this->content->items = array();
+            foreach ($applications as $application) {
+                $this->content->items[] = html_writer::tag('a', $application->prospect_title, ['href' => $application->link, 'target' => '_blank', 'class' => 'application-prospect']);
+                $this->content->items[] = html_writer::div($application->status);
+            }
+        }
     }
 }
--- db/access.php
+++ db/access.php
@@ -14,20 +14,20 @@ $capabilities = array(
         'archetypes' => array(
             'user' => CAP_ALLOW
         ),
- 
+
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
     ),
- 
+
     'block/simplehtml:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
- 
+
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
         ),
- 
+
         'clonepermissionsfrom' => 'moodle/site:manageblocks'
     ),
 );
